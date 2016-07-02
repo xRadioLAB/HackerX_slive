c83758041fb7ba550fe23f5311512cede72d6c37
lxvc <403381161@qq.com>
2016-07-02 19:23:34 +0800

    websocket 联通

--- back-end/back-end.py
+++ back-end/back-end.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('message', namespace='/test/')
+def test_message(message):
+    token = request.args.get('token', '')
+    print token
+    print message
+    emit('response', {'data': 'got it!'})
 
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True, host='127.0.0.1')
