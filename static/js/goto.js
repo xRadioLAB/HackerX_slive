8cb71b2e17441901103c2e4d4c592dd5be849a2b
superwoods <st_sister@icloud.com>
2016-07-05 15:51:26 +0800

    Update folder static

--- static/js/goto.js
+++ static/js/goto.js
@@ -1,71 +1,64 @@
-// @St. goto.js
+/**
+ * @project Slive
+ * @authour "HackerX" Tsinghua Team Quincopter
+ *          - John Lee
+ *          - LXVC
+ *          - Super Woods <st_sister@icloud.com>
+ *
+ * @time    2016-07-02-17.41
+ *          2016-07-05-15.15 cleanup
+ */
+
+// goToSlideByIndex(status) status: string['goToPrevious' | 'goToNext']
 function goToSlideByIndex(status) {
-    // var goToFirst = Office.Index.First;
-    // var goToLast = Office.Index.Last;
-    // var goToPrevious = Office.Index.Previous;
-    // var goToNext = Office.Index.Next;
     var active;
-
-    // var dev = false;
-    // var sever = dev ? 'http://127.0.0.1:5000/socket/' : 'http://123.206.42.148:5000/socket/';
-    // var socket = io.connect(sever);
-
-
-    // if (status === 'goToFirst') {
-    //     active = goToFirst;
-    // }
-    // if (status === 'goToLast') {
-    //     active = goToLast;
-    // }
     if (status === 'goToPrevious') {
         active = Office.Index.Previous;
-        // socket.emit('previous');
     }
     if (status === 'goToNext') {
         active = Office.Index.Next;
-        $.get('http://123.206.42.148:5000/flag/?action=next', function(result){
+        $.get('http://123.206.42.148:5000/flag/?action=next', function(result) {
             console.log('get:', result);
         });
     }
     // Office.context.document.goToByIdAsync(active, Office.GoToType.Index, callback(asyncResult));
     Office.context.document.goToByIdAsync(active, Office.GoToType.Index);
 }
-function callback(asyncResult) {
-    if (asyncResult.status == "failed") {
-        $('#text').append("Action failed with error: " + asyncResult.error.message + "<br>");
-    } else {
-        $('#text').append("Navigation successful<br>");
-    }
-}
-// OfficeInit();
-(function OfficeInit() {
+
+// callback() just for dev console logs
+// function callback(asyncResult) {
+//     if (asyncResult.status == "failed") {
+//         $('#text').append("Action failed with error: " + asyncResult.error.message + "<br>");
+//     } else {
+//         $('#text').append("Navigation successful<br>");
+//     }
+// }
+
+// Office initialize
+(function() {
     Office.initialize = function(reason) {
-        // $(function() {
-        $('#text').append("Office.initialize" + reason);
+        // $('#text').append("Office.initialize" + reason);
         goToSlideByIndex('goToPrevious');
         goToSlideByIndex('goToNext');
-        // });
     };
 })();
 
+
 $(function() {
     // onLoadData
-    function onLoadData() {
-        var dev = false;
-        var sever = dev ? 'http://127.0.0.1:5000/socket/' : 'http://123.206.42.148:5000/socket/';
-        var socket = io.connect(sever);
-        socket.on('previous', function(data) {
-            goToSlideByIndex('goToPrevious');
-        });
-        socket.on('next', function(data) {
-            goToSlideByIndex('goToNext');
-        });
-        socket.on('goToFirst', function(data) {
-            goToSlideByIndex('goToFirst');
-        });
-        socket.on('goToLast', function(data) {
-            goToSlideByIndex('goToLast');
-        });
-    }
-    onLoadData();
+    var dev = false;
+    var sever = dev ? 'http://127.0.0.1:5000/socket/' : 'http://123.206.42.148:5000/socket/';
+    var socket = io.connect(sever);
+    socket.on('previous', function(data) {
+        goToSlideByIndex('goToPrevious');
+    });
+    socket.on('next', function(data) {
+        goToSlideByIndex('goToNext');
+    });
+    // socket.on('goToFirst', function(data) {
+    //     goToSlideByIndex('goToFirst');
+    // });
+    // socket.on('goToLast', function(data) {
+    //     goToSlideByIndex('goToLast');
+    // });
 });
--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,15 @@
-// @St. 2016-07-02-17.41
+/**
+ * @project Slive
+ * @authour "HackerX" Tsinghua Team Quincopter
+ *          - John Lee
+ *          - LXVC
+ *          - Super Woods <st_sister@icloud.com>
+ *
+ * @time    2016-07-02-17.41
+ *          2016-07-05-15.15 cleanup
+ */
 "use strict";
 $(function() {
-    var $app = $("#app");
-    var $text = $("#text");
     var myChart = echarts.init(document.getElementById('app'));
     var option = {
         title: {
@@ -23,16 +30,15 @@ $(function() {
         }]
     };
     myChart.setOption(option);
+
     // onLoadData
-    function onLoadData() {
-        var dev = false;
-        var sever = dev ? 'http://127.0.0.1:5000/socket/' : 'http://123.206.42.148:5000/socket/';
-        var socket = io.connect(sever);
-        socket.on('slack', function(data) {
-            option.series[0].data[$.inArray(data.name, option.xAxis.data)] += 1;
-            // console.log(data.name + ':' + option.series[0].data[$.inArray(data.name, option.xAxis.data)]);
-            myChart.setOption(option);
-        });
-    }
-    onLoadData();
+    var dev = false;
+    var sever = dev ? 'http://127.0.0.1:5000/socket/' : 'http://123.206.42.148:5000/socket/';
+    var socket = io.connect(sever);
+    socket.on('slack', function(data) {
+        option.series[0].data[$.inArray(data.name, option.xAxis.data)] += 1;
+        // console.log(data.name + ':' + option.series[0].data[$.inArray(data.name, option.xAxis.data)]);
+        myChart.setOption(option);
+    });
+
 });
